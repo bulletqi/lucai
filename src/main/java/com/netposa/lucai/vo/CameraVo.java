9b65136e35df875bcee6e6e90493ec5fbc4ac762
西安-戚鲁成 <qilucheng@netposa.com>
2018-01-10 11:28:24 +0800

    描述修改

--- src/main/java/com/netposa/lucai/vo/CameraVo.java
+++ src/main/java/com/netposa/lucai/vo/CameraVo.java
@@ -39,6 +39,6 @@ public class CameraVo {
 	@ApiParam("所属分组id")
 	protected Integer group;
 	@ApiParam("一杆多头属性，字符串格式(json形式)" +
-			"例:[{type(摄像机属性):${type},toward(摄像机朝向):${toward}}]")
+			"例:[{type(摄像机类型):${type},toward(摄像机朝向):${toward}}]")
 	protected String attr;
 }
